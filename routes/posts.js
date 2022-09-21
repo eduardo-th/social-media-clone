5a25f446e8b4c6d4252a1cab2f475ffaef876f46
eduardo-th <105067740+eduardo-th@users.noreply.github.com>
2022-09-21 16:34:53 -0300

    feat: add new posts route

--- routes/posts.js
+++ routes/posts.js
@@ -6,6 +6,9 @@ router.get('/', async (req, res) => {
   const foundPost = await Post.find().limit(10).sort({ createdAt: -1 });
   res.render('posts/index', { foundPost });
 });
+router.get('/new', (req, res) => {
+  res.render('posts/new');
+});
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
   const foundPost = await Post.findById(id);
