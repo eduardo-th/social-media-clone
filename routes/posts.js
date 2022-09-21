0bdbea815f96f064d9fde2e62841ce3b0db50669
eduardo-th <105067740+eduardo-th@users.noreply.github.com>
2022-09-21 15:54:32 -0300

    feat: add basic posts.post route userid and image fields are not stored, need multer and session

--- routes/posts.js
+++ routes/posts.js
@@ -14,7 +14,18 @@ router.get('/:id', async (req, res) => {
 router.get('/:id/edit', async (req, res) => {
   const { id } = req.params;
   const foundPost = await Post.findById(id);
-  console.log(foundPost);
   res.render('posts/edit', { foundPost });
+});
+router.post('/', async (req, res) => {
+  const { title, body, tags } = req.body;
+  const postDoc = new Post({
+    title,
+    body,
+    tags: tags.split(' '),
+    //get user id from session
+    //multer image
+  });
+  await postDoc.save();
+  res.redirect('/');
 });
 module.exports = router;
