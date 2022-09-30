8fa640547c1eeb99ea71b4353ae994e88475e884
eduardo-th <105067740+eduardo-th@users.noreply.github.com>
2022-09-30 18:13:21 -0300

    fead: add reference to db seed the references are not unique, is to fill documents in db

--- seed/seed.js
+++ seed/seed.js
@@ -7,6 +7,7 @@ const bodys = require('./bodys');
 const images = require('./images');
 const comments = require('./comments');
 
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const User = require('../models/user');
 const Post = require('../models/post');
@@ -23,18 +24,97 @@ db.once('open', () => {
   console.log('Database Connected');
 });
 
-addUser(10);
-addPost(10);
-addComment(50).then(() => {
+const usersQty = 10;
+const postsQty = 10;
+const commentsQty = 50;
+
+seed(usersQty, postsQty, commentsQty).then(() => {
   db.close();
   console.log('DONE');
 });
+async function seed() {
+  await addUser(usersQty);
+  await addComment(commentsQty);
+  await addPost(postsQty);
+  await fillUser();
+}
+async function fillUser() {
+  const allUsers = await User.find();
+  const allPosts = await Post.find();
+  const bulkSave = [];
 
+  for (let user of allUsers) {
+    const updateDoc = {
+      updateOne: {
+        filter: { _id: user._id },
+        update: {
+          posts: [getRandValue(allPosts), getRandValue(allPosts), getRandValue(allPosts)],
+          follows: [getRandValue(allUsers), getRandValue(allUsers), getRandValue(allUsers)],
+          followers: [getRandValue(allUsers), getRandValue(allUsers), getRandValue(allUsers)],
+        },
+      },
+    };
+    bulkSave.push(updateDoc);
+  }
+  await User.bulkWrite(bulkSave);
+}
+async function addPost(num) {
+  await Post.deleteMany();
+  const allUsers = await User.find();
+  const allComments = await Comment.find();
+  for (let i = 0; i < num; i++) {
+    const title = getRandValue(titles);
+    const body = getRandValue(bodys);
+    const tags = [getRandValue(tagList), getRandValue(tagList), getRandValue(tagList)];
+    const image = getRandValue(images);
+    const author = getRandValue(allUsers);
+    const comments = [
+      getRandValue(allComments),
+      getRandValue(allComments),
+      getRandValue(allComments),
+    ];
+    const postDoc = new Post({
+      title,
+      body,
+      tags,
+      image,
+      author,
+      comments,
+    });
+    await postDoc.save();
+  }
+}
+async function addUser(num) {
+  await User.deleteMany();
+
+  for (let i = 0; i < num; i++) {
+    const username = getRandValue(usernames);
+    const avatar = getRandValue(avatars);
+    const about = getRandValue(abouts);
+    const email = `${username}@gmail.com`;
+
+    const password = username;
+    const salt = getSalt();
+    const hash = await getHash(password, salt);
+
+    const userDoc = new User({
+      username,
+      avatar,
+      about,
+      email,
+      salt,
+      hash,
+    });
+    await userDoc.save();
+  }
+}
 async function addComment(num) {
   await Comment.deleteMany();
+  const allUsers = await User.find();
   for (let i = 0; i < num; i++) {
     const body = getRandValue(comments);
-    const author = mongoose.Types.ObjectId();
+    const randUser = getRandValue(allUsers);
+    const author = randUser._id;
 
     const commentDoc = new Comment({
       body,
@@ -43,41 +123,28 @@ async function addComment(num) {
     await commentDoc.save();
   }
 }
-async function addPost(num) {
-  await Post.deleteMany();
-  for (let i = 0; i < num; i++) {
-    const title = getRandValue(titles);
-    const body = getRandValue(bodys);
-    const tags = [getRandValue(tagList), getRandValue(tagList), getRandValue(tagList)];
-    const image = getRandValue(images);
+function getHash(password, salt) {
+  const encoding = 'hex';
+  const iterations = 25000;
+  const keylen = 512;
+  const algorithm = 'sha256';
 
-    const postDoc = new Post({
-      title,
-      body,
-      tags,
-      image,
+  const hash = new Promise((resolve, reject) => {
+    crypto.pbkdf2(password, salt, iterations, keylen, algorithm, (err, key) => {
+      resolve(key.toString(encoding));
     });
-    await postDoc.save();
-  }
+  });
+  return hash;
 }
-async function addUser(num) {
-  await User.deleteMany();
-  for (let i = 0; i < num; i++) {
-    const username = getRandValue(usernames);
-    const avatar = getRandValue(avatars);
-    const about = getRandValue(abouts);
-    const email = `${username}@gmail.com`;
+function getSalt() {
+  const saltlen = 32;
+  const encoding = 'hex';
 
-    const userDoc = new User({
-      username,
-      avatar,
-      about,
-      email,
-    });
-    await userDoc.save();
-  }
+  const bufferSalt = crypto.randomBytes(saltlen);
+  const salt = bufferSalt.toString(encoding);
+
+  return salt;
 }
-
 function getRandIndex(arr) {
   const index = Math.floor(Math.random() * arr.length);
   return index;
