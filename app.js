6e9b70aa15dcd03d876c91262276acec6b83afa4
eduardo-th <105067740+eduardo-th@users.noreply.github.com>
2022-09-30 22:57:11 -0300

    feat: install ejs-mate add basic boilerplate for templatin

--- app.js
+++ app.js
@@ -32,6 +32,9 @@ const indexRouter = require('./routes/index');
 const postsRouter = require('./routes/posts');
 const usersRouter = require('./routes/users');
 
+const engine = require('ejs-mate');
+app.engine('ejs', engine);
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
