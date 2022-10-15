2858d3f6acb4df97a5517820e7598a0b31beeb10
eduardo-th <105067740+eduardo-th@users.noreply.github.com>
2022-10-15 17:38:40 -0300

    chore: install multer-storage-cloudinary

--- app.js
+++ app.js
@@ -14,6 +14,16 @@ const flash = require('connect-flash');
 const cloudinary = require('cloudinary').v2
 const multer  = require('multer')
 const upload = multer({ dest: 'uploads/' })
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
+
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+});
 
 const User = require('./models/user');
 
