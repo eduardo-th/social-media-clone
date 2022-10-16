621df8974c0f36a85b7c77cdbf45a0d7e29410e2
eduardo-th <105067740+eduardo-th@users.noreply.github.com>
2022-10-16 17:56:11 -0300

    chore: config multer-cloudinary

--- app.js
+++ app.js
@@ -13,17 +13,26 @@ const flash = require('connect-flash');
 
 const cloudinary = require('cloudinary').v2
 const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
-
-
-
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true,
 });
+const multerStorage=new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'social-media/posts',
+    allowed_formats: ['jpg','png','jpeg'],    
+    eager :(req,file)=>{      
+      return {gravity: "center", height: 250, width: 250, crop: "thumb"}
+    }
+  },
+//avatar  {height: 50, radius: 50, width: 50, crop: "fill"}
+//thum    {gravity: "center", height: 200, width: 250, crop: "thumb"}
+});
+const upload = multer({ storage: multerStorage })
 
 const User = require('./models/user');
 
