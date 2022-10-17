5479757bbf82297de2b5b1a1b77111efc98393ec
eduardo-th <105067740+eduardo-th@users.noreply.github.com>
2022-10-17 17:41:32 -0300

    feat: allow user to upload image in post

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/post');
 const middlewares = require('../middlewares');
+const upload = require('../multercloudinaryconfig');
 
 router.get('/', async (req, res) => {
   const foundPost = await Post.find().limit(10).sort({ createdAt: -1 });
@@ -20,26 +21,39 @@ router.get('/:id', async (req, res) => {
 
   if (req.user) {
     userComment = foundPost.comments.find((elem) => elem.author._id.equals(req.user._id));
-  }  
-  res.render('posts/show', { foundPost,userComment });
+  }
+  res.render('posts/show', { foundPost, userComment });
 });
 router.get('/:id/edit', middlewares.isLoggedIn, async (req, res) => {
   const { id } = req.params;
   const foundPost = await Post.findById(id);
   res.render('posts/edit', { foundPost });
 });
-router.post('/', middlewares.validatePost, middlewares.isLoggedIn, async (req, res) => {
-  const { title, body, tags } = req.body;
-  const postDoc = new Post({
-    title,
-    body,
-    tags: tags.split(' '),
-    //get user id from session
-    //multer image
-  });
-  await postDoc.save();
-  res.redirect('/');
-});
+
+router.post(
+  '/',
+  middlewares.isLoggedIn,
+  upload.single('image'),
+  middlewares.validatePost,
+  async (req, res) => {
+    const { title, body, tags } = req.body;
+    const image = {
+      url: req.file.path,
+      filename: req.file.filename,
+    };
+    console.log(req.body);
+    console.log(req.file);
+    const postDoc = new Post({
+      title,
+      body,
+      tags: tags.split(' '),
+      author: req.user._id,
+      image,
+    });
+    await postDoc.save();
+    res.redirect('/');
+  }
+);
 router.patch('/:id', middlewares.isLoggedIn, async (req, res) => {
   const { id } = req.params;
   const { title, body, tags } = req.body;
@@ -47,8 +61,6 @@ router.patch('/:id', middlewares.isLoggedIn, async (req, res) => {
     title,
     body,
     tags: tags.split(' '),
-    //get user id from session
-    //multer image
   };
   const foundDoc = await Post.findByIdAndUpdate(id, editedPost);
   res.redirect(`/posts/${id}`);
