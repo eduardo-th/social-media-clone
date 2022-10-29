2e2ae735c6cb4711778b62d7b7feedea48d626ce
eduardo-th <105067740+eduardo-th@users.noreply.github.com>
2022-10-29 14:29:57 -0300

    feat: add user avatar thumbnail to db

--- models/user.js
+++ models/user.js
@@ -1,45 +1,53 @@
 const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
+const options = { timestamps: true, toObject: { virtuals: true }, toJSON: { virtuals: true } };
 const passportLocalMongoose = require('passport-local-mongoose');
 const pluginOptions = { selectFields: ['username', '_id'] };
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  avatar: {
-    url: String,
-    filename: String,
-  },
-  about: { type: String },
-  posts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Post',
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
-  ],
-  follows: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'User',
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-  ],
-  followers: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'User',
+    avatar: {
+      url: String,
+      filename: String,
     },
-  ],
-  hash: { type: String },
-  salt: { type: String },
+    about: { type: String },
+    posts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Post',
+      },
+    ],
+    follows: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    followers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    hash: { type: String },
+    salt: { type: String },
+  },
+  options
+);
+
+userSchema.virtual('avatar.thumbnail').get(function () {
+  return this.avatar.url.replace('/upload', '/upload/c_fill,g_faces,h_250,r_max,w_250');
 });
 
 userSchema.plugin(passportLocalMongoose, pluginOptions);
