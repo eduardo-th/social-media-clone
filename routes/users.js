4593782469eaefc60f17d28d11d46f229126de60
eduardo-th <105067740+eduardo-th@users.noreply.github.com>
2022-10-29 14:43:02 -0300

    feat: add form to change user ifo/avatar image

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const passport = require('passport');
+const upload = require('../multercloudinaryconfig');
 
 router.get('/', function (req, res, next) {
   res.render('users/register');
@@ -9,12 +10,33 @@ router.get('/', function (req, res, next) {
 router.get('/login', (req, res) => {
   res.render('users/login');
 });
-router.get('/:id', async (req,res)=>{
-  const {id}=req.params
-  const foundUser= await User.findById(id)  
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;  
 
-  res.render('users/show',{foundUser})
-})
+  const foundUser = await User.findById(id);
+
+  console.log(foundUser);
+
+  res.render('users/show', { foundUser });
+});
+router.patch('/:id', upload.single('image'), async (req, res) => {
+  console.log('LLEGO AL PATCH USER');
+  const { id } = req.params;
+  const { about } = req.body;
+  const avatar = {
+    url: req.file.path,
+    filename: req.file.filename,
+  };
+
+  const foundUser = await User.findByIdAndUpdate(id, { about, avatar },{new:true});
+
+  console.log('-*-*PARAMS-*-*', req.params);
+  console.log('-*-*BODY-*-*', req.body);
+  console.log('-*-*FILE-*-*', req.file);
+  console.log('-*-*FOUND USER-*-*', foundUser);
+
+  res.redirect(`/users/${id}`);
+});
 router.post('/', async (req, res) => {
   const { username, password, email } = req.body;
   const userDoc = new User({ username, email });
