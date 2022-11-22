8c7dbf9c95a0b3c2faa196c13d276eb1d043211e
eduardo-th <105067740+eduardo-th@users.noreply.github.com>
2022-11-22 16:30:18 -0300

    style: move posts routes to controller file

---
+++ controllers/posts.js
@@ -0,0 +1,62 @@
+const Post = require('../models/post');
+
+module.exports.getIndexPost = async (req, res) => {
+  const foundPost = await Post.find().limit(10).sort({ createdAt: -1 });
+  res.render('posts/index', { foundPost });
+};
+module.exports.getNewPost = (req, res) => {
+  res.render('posts/new');
+};
+module.exports.getShowPost = async (req, res) => {
+  let userComment = null;
+  const { id } = req.params;
+  const foundPost = await Post.findById(id)
+    .populate('author', 'username')
+    .populate({
+      path: 'comments',
+      populate: { path: 'author', select: 'username', ref: 'User' },
+    });
+
+  if (req.user) {
+    userComment = foundPost.comments.find((elem) => elem.author._id.equals(req.user._id));
+  }
+  res.render('posts/show', { foundPost, userComment });
+};
+module.exports.getEditPost = async (req, res) => {
+  const { id } = req.params;
+  const foundPost = await Post.findById(id);
+  res.render('posts/edit', { foundPost });
+};
+module.exports.createNewPost = async (req, res) => {
+  const { title, body, tags } = req.body;
+  const image = {
+    url: req.file.path,
+    filename: req.file.filename,
+  };
+  const postDoc = new Post({
+    title,
+    body,
+    tags: tags.split(' '),
+    author: req.user._id,
+    image,
+  });
+  await postDoc.save();
+  res.redirect('/');
+};
+module.exports.editPost = async (req, res) => {
+  const { id } = req.params;
+  const { title, body, tags } = req.body;
+  const editedPost = {
+    title,
+    body,
+    tags: tags.split(' '),
+  };
+  const foundDoc = await Post.findByIdAndUpdate(id, editedPost);
+  res.redirect(`/posts/${id}`);
+};
+module.exports.deletePost = async (req, res) => {
+  const { id } = req.params;
+  const foundDoc = await Post.findByIdAndDelete(id);
+
+  res.redirect('/');
+};
--- routes/posts.js
+++ routes/posts.js
@@ -1,74 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const Post = require('../models/post');
 const middlewares = require('../middlewares');
-const {upload} = require('../multercloudinaryconfig');
+const { upload } = require('../multercloudinaryconfig');
+const postController = require('../controllers/posts');
 
-router.get('/', async (req, res) => {
-  const foundPost = await Post.find().limit(10).sort({ createdAt: -1 });
-  res.render('posts/index', { foundPost });
-});
-router.get('/new', middlewares.isLoggedIn, (req, res) => {
-  res.render('posts/new');
-});
-router.get('/:id', async (req, res) => {
-  let userComment = null;
-  const { id } = req.params;
-  const foundPost = await Post.findById(id)
-    .populate('author', 'username')
-    .populate({
-      path: 'comments',
-      populate: { path: 'author', select: 'username', ref: 'User' },
-    });
-
-  if (req.user) {
-    userComment = foundPost.comments.find((elem) => elem.author._id.equals(req.user._id));
-  }
-  res.render('posts/show', { foundPost, userComment });
-});
-router.get('/:id/edit', middlewares.isLoggedIn, async (req, res) => {
-  const { id } = req.params;
-  const foundPost = await Post.findById(id);
-  res.render('posts/edit', { foundPost });
-});
+router.get('/', postController.getIndexPost);
+router.get('/new', middlewares.isLoggedIn, postController.getNewPost);
+router.get('/:id', postController.getShowPost);
+router.get('/:id/edit', middlewares.isLoggedIn, postController.getEditPost);
 
 router.post(
   '/',
   middlewares.isLoggedIn,
   upload.single('image'),
   middlewares.validatePost,
-  async (req, res) => {
-    const { title, body, tags } = req.body;
-    const image = {
-      url: req.file.path,
-      filename: req.file.filename,
-    };
-    const postDoc = new Post({
-      title,
-      body,
-      tags: tags.split(' '),
-      author: req.user._id,
-      image,
-    });
-    await postDoc.save();
-    res.redirect('/');
-  }
+  postController.createNewPost
 );
-router.patch('/:id', middlewares.isLoggedIn, async (req, res) => {
-  const { id } = req.params;
-  const { title, body, tags } = req.body;
-  const editedPost = {
-    title,
-    body,
-    tags: tags.split(' '),
-  };
-  const foundDoc = await Post.findByIdAndUpdate(id, editedPost);
-  res.redirect(`/posts/${id}`);
-});
-router.delete('/:id', middlewares.isLoggedIn, async (req, res) => {
-  const { id } = req.params;
-  const foundDoc = await Post.findByIdAndDelete(id);
-
-  res.redirect('/');
-});
+router.patch('/:id', middlewares.isLoggedIn, postController.editPost);
+router.delete('/:id', middlewares.isLoggedIn, postController.deletePost);
 module.exports = router;
