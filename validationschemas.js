530f70ae7a04faffc354cefcca556965cbd45ada
eduardo-th <105067740+eduardo-th@users.noreply.github.com>
2022-12-05 11:04:10 -0300

    feat: add user register validation schema

--- validationschemas.js
+++ validationschemas.js
@@ -4,3 +4,10 @@ module.exports.postValidSchema = Joi.object({
   title: Joi.string().required().min(3).max(50),
   body: Joi.string().required().min(3).max(1000),
 });
+
+module.exports.registerUserValidation = Joi.object({
+  username: Joi.string().required().alphanum().trim().min(3).max(20),
+  email: Joi.string().required().email().trim(),
+  password: Joi.string().required().min(8).max(20),
+  confirmPassword: Joi.string().required().valid(Joi.ref('password')).messages({'any.only':'"confirmPassword" must be same as password'}),  
+}).options({abortEarly : false})
