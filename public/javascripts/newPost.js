8bb5a2889aeb062415e256860115f4dc17dc1ef0
eduardo-th <105067740+eduardo-th@users.noreply.github.com>
2022-12-13 14:43:08 -0300

    feat: add client-side validation to new post form

--- public/javascripts/newPost.js
+++ public/javascripts/newPost.js
@@ -1,17 +1,8 @@
 const form = document.querySelector('#form');
+const tagContainer = document.querySelector('#tagContainer');
 const tagInput = document.querySelector('#tags');
 
-tagInput.addEventListener('keyup', (event) => {
-  event.stopPropagation();
-  if (event.keyCode === 13) {
-    const tag = tagInput.value;
-    tagInput.value = '';
-    createTag(tag);
-  }
-});
 function createTag(tagText) {
-  const tagContainer = document.querySelector('#tagContainer');
-
   const tagElem = document.createElement('div');
   const span = document.createElement('span');
   const close = document.createElement('button');
@@ -19,20 +10,56 @@ function createTag(tagText) {
   tagElem.classList.add('col-auto', 'badge', 'text-bg-dark', 'p-0', 'ps-1', 'me-1');
   span.textContent = tagText;
   close.classList.add('btn-close', 'btn-close-white', 'align-middle');
+
   close.addEventListener('click', (event) => {
     event.target.parentNode.remove();
   });
   tagElem.append(span, close);
   tagContainer.append(tagElem);
 }
+function isValidTag(tag) {
+  const minCharLength = 3;
+  const maxCharLength = 20;
+  return tag.length >= minCharLength && tag.length <= maxCharLength;
+}
+function countTags() {
+  const allTags = document.querySelectorAll('.badge');
+  if (allTags.length >= 3 && allTags.length <= 10) {
+    tagContainer.classList.remove('is-invalid');
+    return true;
+  } else {
+    tagContainer.classList.add('is-invalid');
+    return false;
+  }
+}
+tagInput.addEventListener('keyup', (event) => {
+  event.stopPropagation();
+  if (event.keyCode === 13) {
+    if (isValidTag(event.target.value)) {
+      const tag = tagInput.value;
+      tagInput.value = '';
+      createTag(tag);
+      tagContainer.classList.remove('is-invalid');
+      event.target.classList.remove('is-invalid');
+      form.classList.remove('was-validated');
+    } else {
+      event.target.classList.add('is-invalid');
+    }
+  }
+});
 form.addEventListener('submit', (event) => {
   event.stopPropagation();
-  const allTags = document.querySelectorAll('.badge');
-  const arrTags = [];
-  allTags.forEach((tag) => {
-    arrTags.push(tag.textContent);
-  });
-  tagInput.value = arrTags.join(',');
+  if (!countTags() || !form.checkValidity()) {
+    event.preventDefault();
+  } else {
+    const allTags = document.querySelectorAll('.badge');
+    const arrTags = [];
+    allTags.forEach((tag) => {
+      arrTags.push(tag.textContent);
+    });
+    tagInput.value = arrTags.join(',');
+  }
+  form.classList.add('was-validated');
 });
 form.addEventListener('keydown', (event) => {
   event.stopPropagation();
