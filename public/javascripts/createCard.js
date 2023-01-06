239d2ce34632a967a6d1bf65695e61e34f57dc40
eduardo-th <105067740+eduardo-th@users.noreply.github.com>
2023-01-06 16:54:13 -0300

    feat: add infinite scroll to index post page

---
+++ public/javascripts/createCard.js
@@ -0,0 +1,36 @@
+export const createCard = (post) => {
+  const card = document.createElement('div');
+  const row = document.createElement('div');
+  const imgCol = document.createElement('div');
+  const img = document.createElement('img');
+
+  const bodyCol = document.createElement('div');
+  const body = document.createElement('div');
+  const title = document.createElement('h5');
+  const link = document.createElement('a');
+  const textBody = document.createElement('p');
+
+  card.classList.add('card', 'mb-3', 'p-0');
+  row.classList.add('row', 'g-0');
+  imgCol.classList.add('col-md-4');
+  img.classList.add('img-fluid', 'rounded-start');
+  img.src = post.image.thumbnail;
+
+  bodyCol.classList.add('col-md-8');
+  body.classList.add('card-body', 'py-0');
+  title.classList.add('card-title');
+  link.innerText = post.title;
+  link.href = `/posts/${post.id}`;
+  textBody.classList.add('card-text', 'mb-0', 'crop-text-4lines');
+  textBody.innerText = post.body;
+
+  title.appendChild(link);
+  body.append(title, textBody);
+  bodyCol.appendChild(body);
+  imgCol.appendChild(img);
+  row.append(imgCol, bodyCol);
+  card.appendChild(row);
+
+  const cardContainer = document.querySelector('#card-container');
+  cardContainer.appendChild(card);
+};
---
+++ public/javascripts/indexPost.js
@@ -0,0 +1,31 @@
+import { createCard } from './createCard.js';
+
+let currentPage = 0;
+let moreData = true;
+const url = '/posts/feed';
+
+async function getPosts(url, params) {
+  const response = await axios.get(url, { params });
+  return response.data.foundPost;
+}
+window.onscroll = function (ev) {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight && moreData) {
+    let params = { page: currentPage + 1 };
+
+    getPosts(url, params)
+      .then((posts) => {
+        posts.forEach((post) => {
+          createCard(post);
+        });
+        if (posts.length < 10) {
+          showAlertData();
+          moreData = false;
+        }
+        currentPage++;
+      })
+      .catch((err) => console.log(err.message));
+  }
+};
+const showAlertData = () => {
+  document.querySelector('.alert-secondary').classList.remove('d-none');
+};
