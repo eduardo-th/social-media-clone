31a99425a4afeb48373ce223d41032cf18e669dc
eduardo-th <105067740+eduardo-th@users.noreply.github.com>
2023-02-05 11:46:57 -0300

    feat: add html-sanitize to validations

--- validationschemas.js
+++ validationschemas.js
@@ -1,14 +1,37 @@
-const Joi = require('joi');
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension)
 
 module.exports.postValidSchema = Joi.object({
-  title: Joi.string().required().trim().min(3).max(50),
-  body: Joi.string().required().trim().min(3).max(1000),    
-  tags: Joi.array().required().min(1).max(10).items(Joi.string().trim().min(3).max(20))
+  title: Joi.string().required().trim().escapeHTML().min(3).max(50),
+  body: Joi.string().required().trim().escapeHTML().min(3).max(1000),
+  tags: Joi.array().required().min(1).max(10).items(Joi.string().escapeHTML().trim().min(3).max(20))
 }).options({abortEarly : false});
 
 module.exports.registerUserValidation = Joi.object({
-  username: Joi.string().required().alphanum().trim().min(3).max(20),
-  email: Joi.string().required().email().trim(),
-  password: Joi.string().required().min(8).max(20),
-  confirmPassword: Joi.string().required().valid(Joi.ref('password')).messages({'any.only':'"confirmPassword" must be same as password'}),  
+  username: Joi.string().required().alphanum().trim().min(3).max(20).escapeHTML(),
+  email: Joi.string().required().email().trim().escapeHTML(),
+  password: Joi.string().required().min(8).max(20).escapeHTML(),
+  confirmPassword: Joi.string().required().escapeHTML().valid(Joi.ref('password')).messages({'any.only':'"confirmPassword" must be same as password'}),
 }).options({abortEarly : false})
